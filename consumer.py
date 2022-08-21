67df54b230fed06fce05c281e5a006bf776b53ae
Mehmet <mehmet.ersoy1418@gmail.com>
2022-08-21 21:00:52 +0300

    argparse module has been added

--- consumer.py
+++ consumer.py
@@ -2,29 +2,38 @@ import boto3
 import json
 from datetime import datetime
 import time
+import argparse
 
-my_stream_name = 'my-awesome-stream'
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--stream', required=True, type=str)
+    parser.add_argument('--region', required=True, type=str)
+    args = parser.parse_args()
 
-kinesis_client = boto3.client('kinesis', region_name='eu-central-1')
+    my_stream_name = args.stream
 
-response = kinesis_client.describe_stream(StreamName=my_stream_name)
 
-my_shard_id = response['StreamDescription']['Shards'][0]['ShardId']
+    kinesis_client = boto3.client('kinesis', region_name=args.region)
 
-# TRIM_HORIZON means start consumer from earliest record
-shard_iterator = kinesis_client.get_shard_iterator(StreamName=my_stream_name,
-                                                      ShardId=my_shard_id,
-                                                      ShardIteratorType='TRIM_HORIZON')
+    response = kinesis_client.describe_stream(StreamName=my_stream_name)
 
-my_shard_iterator = shard_iterator['ShardIterator']
+    my_shard_id = response['StreamDescription']['Shards'][0]['ShardId']
 
-record_response = kinesis_client.get_records(ShardIterator=my_shard_iterator,
-                                              Limit=2)
+    # TRIM_HORIZON means start consumer from earliest record
+    shard_iterator = kinesis_client.get_shard_iterator(StreamName=my_stream_name,
+                                                        ShardId=my_shard_id,
+                                                        ShardIteratorType='TRIM_HORIZON')
 
-while 'NextShardIterator' in record_response:
-    record_response = kinesis_client.get_records(ShardIterator=record_response['NextShardIterator'])
+    my_shard_iterator = shard_iterator['ShardIterator']
 
-    for i in record_response['Records']:
-        print(i)
+    record_response = kinesis_client.get_records(ShardIterator=my_shard_iterator)
 
-    print("-----")
+
+    while 'NextShardIterator' in record_response:
+        record_response = kinesis_client.get_records(ShardIterator=record_response['NextShardIterator'])
+
+        for i in record_response['Records']:
+            print(i)
+
+        print("-----")
+        # time.sleep(15)
--- producer.py
+++ producer.py
@@ -1,18 +1,22 @@
 import boto3
 import json
+import argparse
 from datetime import datetime
 import calendar
 import random
 import time
 
-my_stream_name = 'my-awesome-stream'
-kinesis_client = boto3.client('kinesis', region_name='eu-central-1')
+
 
 DEVICE_TYPES = ['SMART_HOME', 'INDUSTRIAL_SENSOR', 'INDUSTRIAL_ROBOT', 'HEALTHCARE', 'CAR']
 DEVICE_STATUS = ['UP', 'DOWN', 'SHOUTDOWN', 'UNHEALTHY', 'UNKNOWN']
 PARTITION_KEY = "pk"
 
-def put_to_stream(device_id, status_timestamp, device_type, device_status):
+def get_kinesis_client(aws_region):
+    kinesis_client = boto3.client('kinesis', region_name=aws_region)
+    return kinesis_client
+
+def put_to_stream(kinesis_client, stream_name, device_id, status_timestamp, device_type, device_status):
     payload = {
                 'deviceID': str(device_id),
                 'statusTimestamp': str(status_timestamp),
@@ -23,20 +27,28 @@ def put_to_stream(device_id, status_timestamp, device_type, device_status):
     print(payload)
 
     put_response = kinesis_client.put_record(
-                   StreamName=my_stream_name,
+                   StreamName=stream_name,
                    Data=json.dumps(payload),
                    PartitionKey=PARTITION_KEY)
 
     print(put_response)
 
-while True:
-    device_id = random.randint(1, 10000)
-    status_timestamp = datetime.now()
-    device_type = random.choice(DEVICE_TYPES)
-    device_status = random.choices(DEVICE_STATUS, weights=[0.6, 0.1, 0.1, 0.1, 0.1])[0]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--stream', required=True, type=str)
+    parser.add_argument('--region', required=True, type=str)
+    parser.add_argument('--frequency', required=True, type=int)
 
+    args = parser.parse_args()
 
-    put_to_stream(device_id, status_timestamp, device_type, device_status)
+    while True:
+        device_id = random.randint(1, 100000)
+        status_timestamp = datetime.now()
+        device_type = random.choice(DEVICE_TYPES)
+        device_status = random.choices(DEVICE_STATUS, weights=[0.6, 0.1, 0.1, 0.1, 0.1])[0]
 
-    # wait for 5 second
-    time.sleep(15)
+        kinesis_client = get_kinesis_client(args.region)
+        put_to_stream(kinesis_client, args.stream, device_id, status_timestamp, device_type, device_status)
+
+        # wait for 5 second
+        time.sleep(args.frequency)
